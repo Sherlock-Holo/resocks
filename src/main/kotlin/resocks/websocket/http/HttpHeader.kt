678a9dad997323f46cb82606a6133a5f0af4b5c5
Sherlock Holo <sherlockya@gmail.com>
2018-01-06 15:14:52 +0800

    Rewrite HttpHeader

--- src/main/kotlin/resocks/websocket/http/HttpHeader.kt
+++ src/main/kotlin/resocks/websocket/http/HttpHeader.kt
@@ -6,12 +6,11 @@ import java.nio.channels.AsynchronousSocketChannel
 import java.security.MessageDigest
 import java.util.*
 
-class HttpHeader private constructor(isClient: Boolean, value: String) {
-    private val headerBuilder = StringBuilder()
+class HttpHeader {
     private val header: String
-//    private val headerBuffer: By
 
-    init {
+    private constructor(isClient: Boolean, value: String) {
+        val headerBuilder = StringBuilder()
         header = if (isClient) {
             headerBuilder.append("GET /chat HTTP/1.1\r\n")
             headerBuilder.append("Host: github.com\r\n")
@@ -29,25 +28,57 @@ class HttpHeader private constructor(isClient: Boolean, value: String) {
         }
     }
 
-    fun getHeaderText() = header.toByteArray()
+    private constructor(headerCache: ByteArray) {
+        header = String(headerCache)
+    }
+
+    fun getHeaderByteArray() = header.toByteArray()
+
+    fun checkHttpHeader(): Boolean {
+        // check client handshake
+
+        val headList = header.substring(0 until header.length - 2).split("\r\n")
+
+        if (!headList[0].startsWith("GET")) return false
+        if (headList[2] != "Upgrade: websocket") return false
+        if (headList[3] != "Connection: Upgrade") return false
+        if (headList[5] != "Sec-WebSocket-Version: 13") return false
+
+        return true
+    }
+
+    fun checkHttpHeader(secWebSocketKey: String): Boolean {
+        // check client handshake
+
+        val headList = header.substring(0 until header.length - 2).split("\r\n")
+
+        if (headList[0] != "HTTP/1.1 101 Switching Protocols") return false
+        if (headList[1] != "Upgrade: websocket") return false
+        if (headList[2] != "Connection: Upgrade") return false
+        if (headList[3] != "Sec-WebSocket-Accept: " + genSecWebSocketAccept(secWebSocketKey)) return false
+        return true
+    }
+
 
     companion object {
+        // get server http header
         fun getHttpHeader(secWebSocketKey: String) = HttpHeader(false, genSecWebSocketAccept(secWebSocketKey))
 
+        // get client http header
         fun getHttpHeader(): HttpHeader {
             val array = ByteArray(16)
             Random().nextBytes(array)
             return HttpHeader(true, String(array))
         }
 
-        private fun genSecWebSocketAccept(secWebSocketKey: String): String {
+        fun genSecWebSocketAccept(secWebSocketKey: String): String {
             val sha1 = MessageDigest.getInstance("SHA1")
             sha1.update((secWebSocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").toByteArray())
             return String(Base64.getEncoder().encode(sha1.digest()))
         }
 
-        suspend fun receiveHttpHeader(socket: AsynchronousSocketChannel): ByteArray {
-            val headCache = LinkedList<Byte>()
+        suspend fun receiveHttpHeader(socket: AsynchronousSocketChannel): HttpHeader {
+            val headerCache = LinkedList<Byte>()
             val buffer = ByteBuffer.allocate(4)
             val oldArray = ByteArray(4)
             var nowArray: ByteArray
@@ -56,13 +87,13 @@ class HttpHeader private constructor(isClient: Boolean, value: String) {
             socket.aRead(buffer)
             System.arraycopy(buffer.array(), 0, oldArray, 0, 4)
             buffer.clear()
-            headCache.addAll(oldArray.asIterable())
+            headerCache.addAll(oldArray.asIterable())
 
             loop@ while (true) {
                 haveRead = socket.aRead(buffer)
                 buffer.flip()
                 nowArray = buffer.array()
-                headCache.addAll(nowArray.asIterable())
+                headerCache.addAll(nowArray.asIterable())
                 when (haveRead) {
                     4 -> {
                         if (nowArray.contentEquals("\r\n\r\n".toByteArray())) break@loop
@@ -78,34 +109,8 @@ class HttpHeader private constructor(isClient: Boolean, value: String) {
                 }
                 buffer.clear()
             }
-            return headCache.toByteArray()
-        }
-
-        fun checkHttpHeader(headCache: ByteArray): Boolean {
-            // check client handshake
-            
-            val headString = String(headCache)
-            val headList = headString.substring(0 until headString.length - 2).split("\r\n")
-
-            if (!headList[0].startsWith("GET")) return false
-            if (headList[2] != "Upgrade: websocket") return false
-            if (headList[3] != "Connection: Upgrade") return false
-            if (headList[5] != "Sec-WebSocket-Version: 13") return false
-
-            return true
-        }
-
-        fun checkHttpHeader(headCache: ByteArray, secWebSocketKey: String): Boolean {
-            // check client handshake
-
-            val headString = String(headCache)
-            val headList = headString.substring(0 until headString.length - 2).split("\r\n")
-
-            if (headList[0] != "HTTP/1.1 101 Switching Protocols") return false
-            if (headList[1] != "Upgrade: websocket") return false
-            if (headList[2] != "Connection: Upgrade") return false
-            if (headList[3] != "Sec-WebSocket-Accept: " + genSecWebSocketAccept(secWebSocketKey)) return false
-            return true
+//            return headerCache.toByteArray()
+            return HttpHeader(headerCache.toByteArray())
         }
     }
 }
