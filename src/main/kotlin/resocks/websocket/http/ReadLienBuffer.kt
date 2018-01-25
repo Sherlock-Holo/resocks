ceb25fd21b88839aa111d2ab0ed7592a9d51451f
Sherlock Holo <sherlockya@gmail.com>
2018-01-26 00:32:59 +0800

    Delete LinkedListChannel

--- src/main/kotlin/resocks/websocket/http/ReadLienBuffer.kt
+++ src/main/kotlin/resocks/websocket/http/ReadLienBuffer.kt
@@ -1,43 +1,34 @@
 package resocks.websocket.http
 
-import kotlinx.coroutines.experimental.channels.LinkedListChannel
 import kotlinx.coroutines.experimental.nio.aRead
+import resocks.websocket.WebsocketException
 import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
 
 class ReadLienBuffer(private val socketChannel: AsynchronousSocketChannel, private val buffer: ByteBuffer) {
-    private val readChannel = LinkedListChannel<Byte>()
     private val builder = StringBuilder()
 
     suspend fun readLine(): String {
         builder.delete(0, builder.length)
         while (true) {
-            if (socketChannel.aRead(buffer) <= 0) TODO("maybe throw exception?")
+            if (socketChannel.aRead(buffer) <= 0) throw WebsocketException("read http header failed")
 
             buffer.flip()
 
-            while (buffer.hasRemaining()) readChannel.offer(buffer.get())
-            buffer.clear()
+            loop@ while (buffer.hasRemaining()) {
+                val byte = buffer.get()
+                when (byte) {
+                    "\r".toByte() -> continue@loop
 
-            loop@ while (true) {
-                val byte = readChannel.poll()
-                if (byte != null) {
-                    when (byte) {
-                        "\r".toByte() -> continue@loop
-
-                        "\n".toByte() -> {
-                            while (true) {
-                                val writeBackByte = readChannel.poll()
-
-                                if (writeBackByte != null) buffer.put(writeBackByte)
-                                else return builder.toString()
-                            }
-                        }
-
-                        else -> builder.append(byte)
+                    "\n".toByte() -> {
+                        buffer.compact()
+                        return builder.toString()
                     }
-                } else continue
+
+                    else -> builder.append(byte)
+                }
             }
+            buffer.clear()
         }
     }
 }
