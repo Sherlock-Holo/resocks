8a3352cf6e0819272622bc7fd48c62aa3244e864
Sherlock Holo <sherlockya@gmail.com>
2018-01-26 21:37:37 +0800

    Add ReadsBuffer

--- src/main/kotlin/resocks/readsBuffer/ReadsBuffer.kt
+++ src/main/kotlin/resocks/readsBuffer/ReadsBuffer.kt
@@ -1,16 +1,25 @@
 package resocks.readsBuffer
 
 import kotlinx.coroutines.experimental.nio.aRead
+import kotlinx.coroutines.experimental.withTimeout
 import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
 
-class ReadsBuffer(private val socketChannel: AsynchronousSocketChannel) {
+class ReadsBuffer(val socketChannel: AsynchronousSocketChannel) {
     private val buffer = ByteBuffer.allocate(8192)
     private var bufferContentLength = 0
 
-    suspend fun reads(length: Int): ByteArray {
-        while (bufferContentLength < length) {
-            bufferContentLength += socketChannel.aRead(buffer)
+    suspend fun reads(length: Int, timeout: Int = -1): ByteArray {
+        when (timeout) {
+            -1 -> {
+                while (bufferContentLength < length) {
+                    bufferContentLength += socketChannel.aRead(buffer)
+                }
+            }
+
+            else -> {
+                withTimeout(timeout) { bufferContentLength += socketChannel.aRead(buffer) }
+            }
         }
 
         val byteArray = ByteArray(length)
