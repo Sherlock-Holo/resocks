4c8aa8f435b36ed17d44b12f298e863a8024b541
Sherlock Holo <sherlockya@gmail.com>
2018-01-28 21:16:26 +0800

    Add text frame test

--- src/test/kotlin/resocks/websocket/connection/TestClient.kt
+++ src/test/kotlin/resocks/websocket/connection/TestClient.kt
@@ -1,11 +1,12 @@
 package resocks.websocket.connection
 
 import kotlinx.coroutines.experimental.runBlocking
+import resocks.websocket.frame.FrameContentType
 
 fun main(args: Array<String>) = runBlocking {
     val client = ClientConnection("127.0.0.2", 5678)
     client.connect()
-    client.putFrame("sherlock".toByteArray())
+    client.putFrame("sherlock".toByteArray(), FrameContentType.TEXT)
     client.putFrame("holo".toByteArray())
     println(String(client.getFrame().content))
     println(String(client.getFrame().content))
