4f24d15f7056aaa19b4ade23586b59e2d97fc8d8
Sherlock Holo <sherlockya@gmail.com>
2018-01-28 17:38:24 +0800

    Test connection again

--- src/test/kotlin/resocks/websocket/connection/TestServer.kt
+++ src/test/kotlin/resocks/websocket/connection/TestServer.kt
@@ -1,13 +1,12 @@
 package resocks.websocket.connection
 
 import kotlinx.coroutines.experimental.runBlocking
-import java.nio.ByteBuffer
 
 fun main(args: Array<String>) = runBlocking {
     val server = ServerConnection.start("127.0.0.2", 5678) {
-        val frame = it.getFrame()
-        println(frame.contentType)
-        val port = ByteBuffer.wrap(frame.content).int
-        println(port)
+        println(String(it.getFrame().content))
+        println(String(it.getFrame().content))
+        it.putFrame("嗷呜~".toByteArray())
+        it.putFrame("嗷呜~".toByteArray())
     }
 }
