d7c355ceb602247f0475e7ea82afacbed4f23fa0
Sherlock Holo <sherlockya@gmail.com>
2018-01-30 22:12:51 +0800

    Add remove

--- src/main/kotlin/resocks/proxy/ResocksConnectionPool.kt
+++ src/main/kotlin/resocks/proxy/ResocksConnectionPool.kt
@@ -4,20 +4,6 @@ import java.nio.channels.AsynchronousSocketChannel
 import java.util.concurrent.ConcurrentHashMap
 
 class ResocksConnectionPool {
-    /*private val capacity = 6
-    private val pool = BooleanArray(capacity) { true }
-
-    @Synchronized
-    fun addCoon(id: Int) {
-        if (id !in 0 until capacity || !pool[id]) throw ProxyException("illegal id")
-        pool[id] = false
-    }
-
-    fun removeConn(id: Int) {
-        if (id !in 0 until capacity || pool[id]) throw ProxyException("illegal id")
-        pool[id] = true
-    }*/
-
     private val capacity = 6
     private val pool = ConcurrentHashMap<Int, AsynchronousSocketChannel>(capacity)
 
@@ -29,5 +15,7 @@ class ResocksConnectionPool {
         return pool[id] ?: throw ProxyException("error id")
     }
 
-    fun remove(id: Int) = pool.remove(id)
+    fun remove(id: Int): AsynchronousSocketChannel {
+        return pool.remove(id) ?: throw ProxyException("error id")
+    }
 }
