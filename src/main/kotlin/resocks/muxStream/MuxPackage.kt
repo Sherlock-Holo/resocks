390e9360ca70bd1409ec13a8ec44a35d70df156d
Sherlock Holo <sherlockya@gmail.com>
2018-02-02 20:40:44 +0800

    Rename

--- src/main/kotlin/resocks/muxStream/MuxPackage.kt
+++ src/main/kotlin/resocks/muxStream/MuxPackage.kt
@@ -1,28 +1,28 @@
 package resocks.muxStream
 
-class MuxPackage(val id: Int, val control: PackageControl, val data: ByteArray? = null) {
+class MuxPackage(val id: Int, val controlMux: MuxPackageControl, val data: ByteArray? = null) {
     val packageByteArray =
             if (data != null) ByteArray(1 + data.size)
             else ByteArray(1)
 
     init {
         var header = id shl 4
-        when (control) {
-            PackageControl.RUNNING -> {
+        when (controlMux) {
+            MuxPackageControl.RUNNING -> {
                 header = header or 1
                 System.arraycopy(data, 0, packageByteArray, 1, data!!.size)
             }
 
-            PackageControl.CONNECT -> {
+            MuxPackageControl.CONNECT -> {
                 header = header or 2
                 System.arraycopy(data, 0, packageByteArray, 1, data!!.size)
             }
 
-            PackageControl.CLOSE1 -> {
+            MuxPackageControl.CLOSE1 -> {
                 header = header or 3
             }
 
-            PackageControl.CLOSE2 -> {
+            MuxPackageControl.CLOSE2 -> {
                 header = header or 4
             }
         }
@@ -39,34 +39,34 @@ class MuxPackage(val id: Int, val control: PackageControl, val data: ByteArray? 
             val header = rawData[0].toInt() and 0xff
             val id = header and 0xf0
 
-            val packageType: PackageControl
+            val muxPackageType: MuxPackageControl
             var data: ByteArray? = null
 
             when (header) {
                 in runningArray -> {
-                    packageType = PackageControl.RUNNING
+                    muxPackageType = MuxPackageControl.RUNNING
                     data = ByteArray(rawData.size - 1)
                     System.arraycopy(rawData, 1, data, 0, data.size)
                 }
 
                 in connectArray -> {
-                    packageType = PackageControl.CONNECT
+                    muxPackageType = MuxPackageControl.CONNECT
                     data = ByteArray(rawData.size - 1)
                     System.arraycopy(rawData, 1, data, 0, data.size)
                 }
 
                 in close1Array -> {
-                    packageType = PackageControl.CLOSE1
+                    muxPackageType = MuxPackageControl.CLOSE1
                 }
 
                 in close2Array -> {
-                    packageType = PackageControl.CLOSE2
+                    muxPackageType = MuxPackageControl.CLOSE2
                 }
 
                 else -> throw MuxException("error package header")
             }
 
-            return MuxPackage(id, packageType, data)
+            return MuxPackage(id, muxPackageType, data)
         }
     }
 }
