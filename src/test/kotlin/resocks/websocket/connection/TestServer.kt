c102a272b660b90b63f664711a07950232e41af3
Sherlock Holo <sherlockya@gmail.com>
2018-02-04 21:43:00 +0800

    Update test

--- src/test/kotlin/resocks/websocket/connection/TestServer.kt
+++ src/test/kotlin/resocks/websocket/connection/TestServer.kt
@@ -1,12 +1,13 @@
 package resocks.websocket.connection
 
 import kotlinx.coroutines.experimental.runBlocking
+import resocks.websocket.frame.FrameContentType
 
-fun main(args: Array<String>) = runBlocking {
-    val server = ServerConnection.start("127.0.0.2", 5678) {
-        println(String(it.getFrame().content))
-        println(String(it.getFrame().content))
-        it.putFrame("嗷呜~".toByteArray())
-        it.putFrame("嗷呜~".toByteArray())
-    }
+fun main(args: Array<String>) = runBlocking<Unit> {
+    ServerConnection.startServer(5678, "127.0.0.2")
+    val server = ServerConnection.getClient()
+    println(String(server.getFrame().content))
+    println(String(server.getFrame().content))
+    server.putFrame("big".toByteArray(), FrameContentType.TEXT)
+    server.putFrame("wolf".toByteArray(), FrameContentType.TEXT)
 }
